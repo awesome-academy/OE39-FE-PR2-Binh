3ee75ffbbb1945980a9ef8fdbec973696472ccb6
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-10-06 22:46:23 +0700

    Create Rating And Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Product from './components/Features/Product';
 import Footer from './components/Partials/Footer/Footer';
 import Header from './components/Partials/Header/Header';
 
@@ -6,6 +7,15 @@ function App() {
   return (
     <>
       <Header />
+      <div className="container mt-4 mb-4">
+        <div className="row">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
+            <div key={index} className="col-lg-3 col-md-4 col-xs-6">
+              <Product />
+            </div>
+          ))}
+        </div>
+      </div>
       <Footer />
     </>
   );
