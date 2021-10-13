69119e4927e1946b2c44b4ea178632bc9c4e7bf7
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-10-13 14:27:13 +0700

    Update Implement Signin and Signup Backend

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { createSampleUsers } from '../controllers/userController.js';
+import { createSampleUsers, signin, signup } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 // Create sample users
 userRouter.get('/seed', createSampleUsers);
+
+// Signup user
+userRouter.post('/signup', signup);
+
+// Signin user
+userRouter.post('/signin', signin);
 
 export default userRouter;
