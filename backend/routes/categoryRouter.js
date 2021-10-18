3d4a33a51fad55530877daf2ffe16f80f7699b10
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-10-18 22:56:32 +0700

    Implement CRUD Category Backend

--- backend/routes/categoryRouter.js
+++ backend/routes/categoryRouter.js
@@ -1,9 +1,32 @@
 import express from 'express';
-import { createSampleCategories } from '../controllers/categoryController.js';
+import {
+  createCategory,
+  createSampleCategories,
+  deleteCategory,
+  getAllCategories,
+  getCategoryBySlug,
+  UpdateCategory,
+} from '../controllers/categoryController.js';
+import { isAdmin, isAuth } from '../utils/authMiddleware.js';
 
 const categoryRouter = express.Router();
 
 // Create sample categories
 categoryRouter.get('/seed', createSampleCategories);
+
+// Add new category
+categoryRouter.post('/', isAuth, isAdmin, createCategory);
+
+// Get category by slug
+categoryRouter.get('/:slug', getCategoryBySlug);
+
+// Get all category
+categoryRouter.get('/', getAllCategories);
+
+// Update category
+categoryRouter.put('/:slug', isAuth, isAdmin, UpdateCategory);
+
+// Delete category
+categoryRouter.delete('/:slug', isAuth, isAdmin, deleteCategory);
 
 export default categoryRouter;
